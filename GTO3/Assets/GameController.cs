73c5916635fc8bc12977d8fe112056e718189d23
Milton van de Sanden <milton.vandesanden@gmail.com>
2017-01-20 19:15:25 +0100

    stuff

---
+++ GTO3/Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    public Vector2 lane1_1;
+    public Vector2 lane1_2;
+    public Vector2 lane1_3;
+    public Vector2 lane2_1;
+    public Vector2 lane2_2;
+    public Vector2 lane2_3;
+    public Vector2 lane3_1;
+    public Vector2 lane3_2;
+    public Vector2 lane3_3;
+
+    // Use this for initialization
+    void Start(){}
+	
+	// Update is called once per frame
+	void Update(){}
+}
---
+++ GTO3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameController gameController;
+
+	// Use this for initialization
+	void Start()
+    {
+        this.transform.position.Set(gameController.lane2_2.x, gameController.lane2_2.y, 0);
+    }
+	
+	// Update is called once per frame
+	void Update(){}
+}
